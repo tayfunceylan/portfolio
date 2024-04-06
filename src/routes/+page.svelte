03803c928a55052a798e98ff2bb9ad55330ae1de
Tayfun <tayfun.ceylan@outlook.de>
2024-04-06 16:46:03 +0000

    - added tailwind to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Homepage incoming...</h1>
+<div class="text-3xl font-bold">Homepage incoming...</div>
 <p>
 	Currently, I am working on this project. Please check back later. <br />
 	This is my initial commit.
