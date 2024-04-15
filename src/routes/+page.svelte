77d1993f53d4d8fe9a0b5f1848ec4de5cf4768aa
Tayfun <tayfun.ceylan@outlook.de>
2024-04-15 23:23:33 +0000

    - added jobs section - added scrollbar-none class to global app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,62 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Section from './Section.svelte';
+
+	const jobs = [
+		{
+			company: 'ZPP INGENIEURE',
+			title: 'Werkstudent IT-Security',
+			when: 'April 2022 - today',
+			href: 'https://zpp.de/',
+			points: [
+				'looking for security issues, bugs and unwanted behaviour',
+				'implementing security measures',
+				'involved in web development',
+				'working with Java, Python and Docker',
+				'occasionally some cool smaller projects'
+			]
+		},
+		{
+			company: 'Opel',
+			title: 'Kommissionierer',
+			when: 'October - December 2020, September - October 2021',
+			href: 'https://opel.de/',
+			points: [
+				'I was working full time for a month and the rest in part time',
+				'I was involved in a lot of different tasks for both Teileeingang and Teileausgang',
+				'driving an EZS 350 (Schlepper) - collecting items in cages attached to the vehicle',
+				'Nachschübe intern - changing location of items internally',
+				'Nachschübe extern - changing location of items to an external area',
+				'Greifen - picking items for costumer delivery',
+				'Konsolidierung - preparing cages for shipment',
+				'Umlagern - manually changing location of items',
+				'Einlagern - store items on a transport trolley coming in from a truck'
+			]
+		},
+		{
+			company: 'Burger King',
+			title: 'Fahrer',
+			when: 'January 2019 - April 2020',
+			href: 'https://burgerking.de/',
+			points: [
+				'most of the time I was delivering burgers to costumers homes in a Toyota Aygo',
+				'I enjoyed listing to podcasts while driving to the customer',
+				'as well as working in the kitchen',
+				'making all kinds of burgers'
+			]
+		},
+		{
+			company: 'M.S.D.',
+			title: 'Zusteller',
+			when: '2013 - 2017',
+			points: [
+				'when I turned 13 I decided to earn some extra money for myself',
+				'at first I was working for zusteller-online.de delivering magazines like TV-Movie or Bravo on my bike',
+				'later on I saw that I had better opportunities at M.S.D. to develop myself, so i applied to M.S.D.',
+				'being outside, enjoying fresh air and hearing music was refreshing'
+			]
+		}
+	];
 </script>
 
 <Navbar />
@@ -25,7 +81,7 @@
 	</p>
 	<a
 		href="https://github.com/tayfunceylan"
-		class="mt-7 text-center rounded-md border-2 border-mint-tulip-500 p-4 font-mono text-mint-tulip-500/80 transition-colors duration-500 hover:bg-mint-tulip-500/20 hover:text-mint-tulip-400"
+		class="mt-7 rounded-md border-2 border-mint-tulip-500 p-4 text-center font-mono text-mint-tulip-500/80 transition-colors duration-500 hover:bg-mint-tulip-500/20 hover:text-mint-tulip-400"
 	>
 		Check out my GitHub!
 	</a>
@@ -79,15 +135,46 @@
 </Section>
 
 <Section title="Where I’ve Worked" id="jobs">
-	<p class="text-center">
-		Currently I'm working as a student assistant... Lorem ipsum dolor sit amet, consetetur
-		sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-		erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
-		kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-		amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-		magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-		Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-	</p>
+	<div class="relative">
+		<div class="scrollbar-none flex overflow-scroll">
+			{#each jobs as { company, title, when, href, points }, i}
+				<div
+					class="h-9 shrink-0 basis-36 flex-nowrap truncate text-nowrap pt-2 text-center font-mono text-sm transition-colors has-[:checked]:bg-mint-tulip-500/20"
+				>
+					<input type="radio" name="company" id={company} class="peer hidden" checked={i === 0} />
+					<label for={company} class="h-5 transition-colors peer-checked:text-mint-tulip-500">
+						{company}
+					</label>
+					<div
+						class="mt-1 h-1 w-full translate-y-3/4 bg-mint-tulip-500 transition-transform duration-1000 ease-in-out peer-checked:translate-y-0"
+					/>
+					<div
+						class="absolute left-0 translate-y-1/4 space-y-3 text-wrap py-5 text-left font-sans
+							opacity-0 transition-all duration-1000 ease-in-out peer-checked:translate-y-0 peer-checked:opacity-100
+						"
+					>
+						<div>
+							<div class="flex flex-wrap items-center space-x-1 text-lg">
+								<div class="font-semibold">{title}</div>
+								<a {href} class="text-mint-tulip-500">@{company}</a>
+							</div>
+							<div class="text-sm">{when}</div>
+						</div>
+						<div class="space-y-1">
+							{#each points as point}
+								<div class="flex gap-2">
+									<div class=" my-auto text-mint-tulip-500">▹</div>
+									<div>
+										{point}
+									</div>
+								</div>
+							{/each}
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
 </Section>
 
 <Section title="What I’ve Built" id="projects">
