03d621b07fafb94b56cae8f3b2481819adb9b496
Tayfun <tayfun.ceylan@outlook.de>
2024-04-16 20:30:34 +0000

    - added 00 section - nav bar bug fixes

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import Logo from '$lib/svg/Logo.svelte';
 
-	let open = false;
+	let open = true;
 	const toggle = () => (open = !open);
-	const sections = ['About', 'Jobs', 'Projects', 'Contact'];
+	const sections = ['Hi', 'About', 'Jobs', 'Projects', 'Contact'];
 </script>
 
 <nav
 	class="fixed left-1/2 top-0 z-10 flex w-full max-w-screen-lg -translate-x-1/2 items-center justify-between bg-blue-zodiac-800 px-7 py-4 shadow-xl shadow-blue-zodiac-800 transition-all duration-1000 md:px-9"
 >
-	<a href="/" class="text-4xl font-semibold text-mint-tulip-500">T</a>
-	<label class="peer relative size-10 text-mint-tulip-500 md:hidden">
+	<a href="/" class="z-10 text-4xl font-semibold text-mint-tulip-500">T</a>
+	<label for="navbar" class="peer relative size-10 text-mint-tulip-500 md:hidden">
+		<div class="absolute z-20 size-14"></div>
 		<div class="absolute left-3/4 top-1/2">
-			<input type="checkbox" class="peer hidden" bind:checked={open} />
+			<input type="checkbox" id="navbar" class="peer hidden" bind:checked={open} />
 			<span
 				class="absolute z-20 h-1 w-7 -translate-x-[calc(50%+7px)] -translate-y-[calc(50%+10px)] scale-x-150 bg-current transition duration-500 ease-in
 						peer-checked:-translate-x-1/2 peer-checked:-translate-y-1/2
@@ -34,47 +35,41 @@
 					"
 			></span>
 			<div
-				class="group pointer-events-none fixed left-0 top-0 flex h-screen translate-x-[0vw] flex-nowrap transition duration-1000 peer-checked:-translate-x-[100vw]"
+				class="fixed right-0 top-0 -z-10 h-screen w-screen translate-x-full opacity-0 backdrop-blur transition-opacity duration-500 peer-checked:translate-x-0 peer-checked:opacity-100"
+			></div>
+			<div
+				class="fixed right-0 top-[50vh] z-20 my-auto flex -translate-y-1/2 translate-x-full flex-col items-center justify-center transition duration-500 ease-in-out peer-checked:-translate-x-[calc(37.5vw-50%)]"
 			>
-				<button
-					class="pointer-events-auto h-full w-[125vw] scale-0 opacity-0 backdrop-blur-sm transition-opacity duration-1000
-						peer-checked:group-[]:scale-100
-						peer-checked:group-[]:opacity-100
-					"
-					onclick={toggle}
-				/>
-				<div
-					class="pointer-events-none flex h-full w-[75vw] flex-col items-center justify-center bg-blue-zodiac-700 ease-out"
-				>
-					{#each sections as section, i}
-						<a
-							href="#{section.toLowerCase()}"
-							class="group/section pointer-events-auto space-y-0.5 py-4"
-							onclick={toggle}
+				{#each sections as section, i}
+					<a
+						href={i != 0 ? `#${section.toLowerCase()}` : ''}
+						class="group/section space-y-0.5 py-4"
+						onclick={toggle}
+					>
+						<div class="text-center font-mono text-lg text-mint-tulip-500">
+							{i.toString().padStart(2, '0')}.
+						</div>
+						<div
+							class="text-center font-mono text-lg text-slate-400 transition duration-300 group-hover/section:text-mint-tulip-500"
 						>
-							<div class="text-center font-mono text-lg text-mint-tulip-500">
-								{(i + 1).toString().padStart(2, '0')}.
-							</div>
-							<div
-								class="text-center font-mono text-lg text-slate-400 transition duration-300 group-hover/section:text-mint-tulip-500"
-							>
-								{section}
-							</div>
-						</a>
-					{/each}
-				</div>
+							{section}
+						</div>
+					</a>
+				{/each}
 			</div>
 		</div>
 	</label>
+	<div
+		class="fixed right-0 top-0 z-10 h-screen w-9/12 translate-x-full bg-blue-zodiac-700 transition duration-500 ease-in-out peer-has-[:checked]:translate-x-0 md:hidden"
+	/>
 	<div class="hidden md:flex">
 		{#each sections as section, i}
 			<a
-				href="#{section.toLowerCase()}"
-				class="group pointer-events-auto flex items-center space-x-1 px-3"
-				onclick={toggle}
+				href={i != 0 ? `#${section.toLowerCase()}` : ''}
+				class="group flex items-center space-x-1 px-3"
 			>
 				<div class="text-center font-mono text-sm text-mint-tulip-500">
-					{(i + 1).toString().padStart(2, '0')}.
+					{i.toString().padStart(2, '0')}.
 				</div>
 				<div
 					class="text-center font-mono text-sm text-slate-300 transition duration-300 group-hover:text-mint-tulip-500"
