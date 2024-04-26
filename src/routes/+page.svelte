06c5d164107a44f91a84e42c4a59e4c572afacfd
Tayfun <tayfun.ceylan@outlook.de>
2024-04-26 13:39:04 +0000

    - fixed fade in effects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,31 +8,44 @@
 	import Socials from './Socials.svelte';
 
 	$effect(() => {
-		const sections = document.querySelectorAll('.observe');
-		const observers: IntersectionObserver[] = [];
-		sections.forEach((section) => {
-			const observer = new IntersectionObserver(
-				(observerEntries) => {
-					observerEntries.forEach((entry) => {
-						if (entry.isIntersecting) {
-							section.classList.remove('opacity-0');
-							section.classList.add('animate-slide-in');
-							observer.disconnect();
-						} else {
-							section.classList.add('opacity-0');
-						}
-					});
-				},
-				{
-					threshold: 0.25
+		const callback = (
+			observerEntries: IntersectionObserverEntry[],
+			observer: IntersectionObserver
+		) => {
+			observerEntries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.remove('opacity-0');
+					entry.target.classList.add('animate-slide-in');
+					observer.unobserve(entry.target);
+				} else {
+					entry.target.classList.add('opacity-0');
 				}
-			);
+			});
+		};
+
+		const observer: IntersectionObserver = new IntersectionObserver(
+			(entries) => callback(entries, observer),
+			{
+				rootMargin: '-0% 0% -25% 0%'
+			}
+		);
+		const observer_100: IntersectionObserver = new IntersectionObserver(
+			(entries) => callback(entries, observer_100),
+			{
+				rootMargin: '-0% 0% -100px 0%'
+			}
+		);
+
+		document.querySelectorAll('.observe').forEach((section) => {
 			observer.observe(section);
-			observers.push(observer);
+		});
+		document.querySelectorAll('.observe-100').forEach((section) => {
+			observer_100.observe(section);
 		});
 
 		return () => {
-			observers.forEach((observer) => observer.disconnect());
+			observer.disconnect();
+			observer_100.disconnect();
 		};
 	});
 </script>
--- src/routes/Socials.svelte
+++ src/routes/Socials.svelte
@@ -6,7 +6,7 @@
 	let xing = 'https://www.xing.com/profile/Tayfun_Ceylan3';
 </script>
 
-<div class="observe mx-auto mb-14 w-fit">
+<div class="observe-100 mx-auto mb-14 w-fit">
 	<div class="mb-2 flex h-6 justify-evenly md:pointer-events-none md:opacity-0">
 		<Github href={github} />
 		<Xing href={xing} />
@@ -15,17 +15,22 @@
 		href={github}
 		class="text-center font-mono text-xs transition duration-500 hover:text-mint-tulip-500"
 		target="_blank"
-		>Built by Tayfun Ceylan
+	>
+		Built by Tayfun Ceylan
 	</a>
 </div>
 
 <div class="hidden md:block">
-	<div class="animate-slide-left fixed bottom-0 left-6 flex w-6 flex-col justify-evenly gap-5 xl:left-14">
+	<div
+		class="fixed bottom-0 left-6 flex w-6 animate-slide-left flex-col items-center justify-evenly gap-5 xl:left-14"
+	>
 		<Github href={github} class="transition hover:-translate-y-1" />
 		<Xing href={xing} class="transition hover:-translate-y-1" />
 		<div class="mx-auto h-24 w-1 rounded bg-slate-500"></div>
 	</div>
-	<div class="animate-slide-right fixed bottom-0 right-6 flex flex-col justify-evenly gap-5 xl:right-14">
+	<div
+		class="fixed bottom-0 right-6 flex animate-slide-right flex-col items-center justify-evenly gap-5 xl:right-14"
+	>
 		<div class="inline-block font-mono [writing-mode:vertical-lr]">Tayfun Ceylan</div>
 		<div class="mx-auto h-24 w-1 rounded bg-slate-500"></div>
 	</div>
